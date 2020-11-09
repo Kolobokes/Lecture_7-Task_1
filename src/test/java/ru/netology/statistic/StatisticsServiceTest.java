777fb534afc8acec7e17e8343c07c796dcbd8af8
Evgeniya Sukhovaya <C226-88-39-D@yandex.ru>
2020-11-09 19:01:21 +0300

    9

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -23,7 +23,7 @@ class StatisticsServiceTest {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 22};
-        long expected = 12;
+        long expected = 22;
 
         long actual = service.findMax(incomesInBillions);
 
